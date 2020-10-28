00cda8692f26665939b6944408c12be11215eb07
Alexcmyk <alex.kirts@gmail.com>
2020-10-28 09:54:03 -0600

    adds jquery functionality

--- main.js
+++ main.js
@@ -1,35 +1,8 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $('.show-filters').on('click', function() {
-    
-//     if($('.filters-list').is(':visible')) {   
-//         $('.filters-list').slideUp()
-//         $('.show-filters').text('Show filters')
-//     } else {
-//         $('.filters-list').slideDown()
-//         $('.show-filters').text('Hide filters')
-//     }
-//     return false
-// })
-
-
-// $('.filters-list a').on('click', function() {
-
-//     let filter = $(this).data('filter')
-//     $('.product').hide()
-//     $(filter).show()
-//     $('.filters-list a').removeClass('selected')
-//     $(this).addClass('selected')
-//     return false
-// })
+$('.filters-list a').on('click', function() {
+    let filter = $(this).data('filter')
+    $('.product').hide()
+    $(filter).show()
+    $('.filters-list a').removeClass('store-selected')
+    $(this).addClass('store-selected')
+    return false
+})
